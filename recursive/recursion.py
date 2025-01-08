d2a062c4d1cd61fd8d8d1e3b91304e7d5809723e
Lcarriersippy <dfclcs@gmail.com>
2025-01-08 14:30:23 +0000

    add solution for the recursion kata

--- recursive/recursion.py
+++ recursive/recursion.py
@@ -1,7 +1,10 @@
 def ghost_buster(ghosts):
     ghost_counter = 0
     for ghost in ghosts:
-        if (ghost['is_ghost']):
+        if type(ghost) == list:
+            ghost_counter += ghost_buster(ghost)
+
+        elif (ghost['is_ghost']):
             ghost_counter+=1
 
     return ghost_counter
--- recursive/test_recursion.py
+++ recursive/test_recursion.py
@@ -1,6 +1,7 @@
 import pytest
 from recursive.recursion import ghost_buster
 from recursive.ghost_data import ghost_data, nested_ghost_data
+import copy
 
 class TestGhostBuster():
     def test_ghost_buster_finds_ghost_when_passed(self):
@@ -8,26 +9,34 @@ class TestGhostBuster():
         found_ghosts = ghost_buster(ghosts)
         assert found_ghosts == 1
 
-    @pytest.mark.skip
+    
     def test_ghost_buster_finds_ghosts_when_passed_multiple(self):
         ghosts = [{'is_ghost': True}, {'is_ghost': True}]
         found_ghosts = ghost_buster(ghosts)
         assert found_ghosts == 2
 
-    @pytest.mark.skip
+
     def test_ghost_buster_ignores_non_ghostly_entities(self):
         ghosts = [{'is_ghost': True}, {'is_ghost': False}]
         found_ghosts = ghost_buster(ghosts)
         assert found_ghosts == 1
 
-    @pytest.mark.skip
+
     def test_ghost_buster_can_deal_with_large_amounts_of_data(self):
         ghosts = ghost_data
         found_ghosts = ghost_buster(ghosts)
         assert found_ghosts == 510
 
-    @pytest.mark.skip
+
     def test_ghost_buster_can_deal_with_nested_data(self):
         ghosts = nested_ghost_data
         found_ghosts = ghost_buster(ghosts)
         assert found_ghosts == 510
+
+    def test_ghost_buster_func_does_not_mutate_original_list(self):
+        ghosts = nested_ghost_data
+        ghosts_copy = copy.deepcopy(ghosts)
+
+        ghost_buster(ghosts)
+
+        assert ghosts == ghosts_copy
