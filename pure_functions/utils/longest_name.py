fffd76dc81c1be37aab5fc9832025347f61e5a6e
Lcarriersippy <dfclcs@gmail.com>
2025-01-08 14:13:10 +0000

    add solution for the purer function kata

--- pure_functions/utils/longest_name.py
+++ pure_functions/utils/longest_name.py
@@ -1,8 +1,11 @@
+import copy
+
 def longest_name(names):
+    names_deep_copy = copy.deepcopy(names)
     name_lengths = []
-    for name in names:
+    for name in names_deep_copy:
         name['length'] = len(name['name'])
         name_lengths.append(name['length'])
     longest_num = max(name_lengths)
     index = name_lengths.index(longest_num)
-    return names[index]['name']
+    return names_deep_copy[index]['name']
